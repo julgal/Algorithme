05677143fbd5789ed42ea594975b8db8149392a1
julgal <gallaisjulienlh76@gmail.com>
2024-02-05 11:01:16 +0100

    Update bubble_sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -3,18 +3,23 @@ def bubble_sort(lst):
 
     for i in range(n - 1):
         flag = 0
-
         for j in range(n - 1):
             if lst[j + 1] < lst[j]:
                 tmp = lst[j]
                 lst[j] = lst[j + 1]
                 lst[j + 1] = tmp
                 flag = 1
-
         if flag == 0:
             break
     return lst
 
-my_list = [1,5,4,3,8,9,45,23,22,67,56,89,7,500,400,600,1000,998,788]
-print(my_list)
-print(bubble_sort(my_list))
+if __name__ == "__main__":
+    try:
+        list_n = []
+        numbers = input("Enter list numbers:")
+        for number in numbers.split():
+            list_n.append(int(number))
+
+        print(bubble_sort(list_n))
+    except:
+        print("Error, You have not enter a number.")
