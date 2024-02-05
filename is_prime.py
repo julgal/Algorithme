20b15c40b9627d05d740405f8e222c049ec29d43
julgal <gallaisjulienlh76@gmail.com>
2024-02-05 11:23:26 +0100

    Update is_pprime

--- is_prime.py
+++ is_prime.py
@@ -5,3 +5,13 @@ def is_prime(nb):
 		if nb % d == 0:
 			return False
 	return True
+
+if __name__ == "__main__":
+    try:
+        number = int(input("Enter a number:"))
+        if is_prime(number):
+            print(f"The number {number} is prime.")
+        else:
+            print(f"The number {number} is not prime.")
+    except:
+        print("Error, you don't have enter a number.")
