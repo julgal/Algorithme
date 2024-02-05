23e3b2296bee021ff828c8c86841750d0702aedf
julgal <gallaisjulienlh76@gmail.com>
2024-02-05 11:13:47 +0100

    Update convert_seconds

--- convert_seconds.py
+++ convert_seconds.py
@@ -5,3 +5,11 @@ def convert_seconds(seconds):
     minutes = seconds // 60
     seconds = seconds - minutes * 60
     return hours, minutes, seconds
+
+if __name__ == "__main__":
+    try:
+        seconds = int(input("Enter the number of seconds:"))
+        result = convert_seconds(seconds)
+        print(f"{result[0]}h {result[1]}m {result[2]}s")
+    except:
+        print("Error, you don't have enter a number.")
