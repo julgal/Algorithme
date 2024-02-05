dceab4cf38989b00a97ae5a574f5b209d2e385df
julgal <gallaisjulienlh76@gmail.com>
2024-02-05 10:49:02 +0100

    Update factorial

--- factorial.py
+++ factorial.py
@@ -6,3 +6,10 @@ def factorial(nb):
 	for num in range(1, nb+1):
 		result *= num
 	return result
+
+if __name__ == "__main__":
+    try:
+        number = int(input("Enter a number: "))
+        print(factorial(number))
+    except:
+        print("Error. You not have enter a number.")
